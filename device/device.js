ece7da8887fbde094363e4dc24b4bd3200f66804
nkg447 <nikunj.gupta447@gmail.com>
2025-01-25 23:20:31 +0530

    pulled js code to seperate files

---
+++ device/device.js
@@ -0,0 +1,64 @@
+function onLoad() {
+  // not smartphone
+  if (
+    !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    document.getElementById("message").innerHTML =
+      "Please open the link in a smartphone.";
+    document.getElementById("caliberate").style.display = "none";
+    return;
+  }
+  // forcing https
+  if (
+    location.protocol !== "https:" &&
+    location.hostname !== "127.0.0.1" &&
+    location.hostname !== "localhost"
+  ) {
+    location.replace(
+      `https:${location.href.substring(location.protocol.length)}`
+    );
+  }
+  window.onerror = (msg, url, lineNum) => {
+    document.getElementById("message").innerHTML = msg;
+  };
+  let id = new URLSearchParams(window.location.search).get("id");
+  const lag = 5;
+  let i = 0;
+  let client = null;
+  client = new WebRTCClient(
+    "https://signallite.onrender.com",
+    id,
+    (event) => {},
+    () => {
+      window.ondeviceorientationabsolute = (e) => {
+        document.getElementById("caliberate").onclick = () => {
+          const data = JSON.stringify({
+            type: "device-caliberate",
+            data: {
+              alpha: e.alpha,
+              beta: e.beta,
+              gamma: e.gamma,
+            },
+          });
+          client.peerChannel.send(data);
+        };
+        i++;
+        if (i % lag == 0) {
+          const data = JSON.stringify({
+            type: "device-data",
+            data: {
+              alpha: e.alpha,
+              beta: e.beta,
+              gamma: e.gamma,
+            },
+          });
+          client.peerChannel.send(data);
+        }
+      };
+    }
+  );
+  client.createOffer();
+}
+onLoad();
